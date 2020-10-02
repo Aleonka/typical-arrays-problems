6cc3b9b3c9752cc8fa735ebee3b0ceb0d0c113dd
Aleonka <70817157+Aleonka@users.noreply.github.com>
2020-10-02 16:27:35 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return 0;
+  }
+let k = array[0];
+for (let i = 1; i < array.length; i++) {
+if (k > array[i]) {
+k =  array[i];
+}
+}
+  return k;
 }
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (array === undefined || array.length == 0) {
+    return 0;
+  }
+  let j = array[0];
+  for (let i = 1; i < array.length; i++) {
+  if (j < array[i]) {
+  j =  array[i];
+  }
+  }
+    return j;
+  }
+
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return 0;
+  }
+  let summa = 0;
+  for (let i = 0; i < array.length; i++) {
+summa = summa + array[i];
+  }
+  return summa / array.length;
 }
